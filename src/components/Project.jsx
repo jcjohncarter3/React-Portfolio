4cca0985372f7db7f3f9eadea584921ea73e3dbb
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-02 22:43:15 -0500

    added symbols and links to the the stuff

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { LogoGithub } from 'react-ionicons'
+import { Eye } from 'react-ionicons'
 
 export default function Project(props) {
     return <div style={{border: "1px solid blue"}}>
@@ -8,8 +10,23 @@ export default function Project(props) {
                 <div style={{border: "1px solid green"}}>
                     <p>{props.title}</p>
                     <div>
-                        <Link to={props.github_link}>G</Link>
-                       {props.live_link ? <Link to={props.live_link}>X</Link>: null}
+                        <Link to={props.github_link}>
+                            <LogoGithub
+                                color={'#00000'} 
+                                title={"GitHub"}
+                                height="25px"
+                                width="25px"
+                            />
+                        </Link>
+                       {props.live_link ? 
+                            <Link to={props.live_link}>
+                                <Eye
+                                    color={'#00000'} 
+                                    title={"Eye"}
+                                    height="25px"
+                                    width="25px"
+                                />
+                            </Link>: null}
                     </div>
                 </div>
             </div>
