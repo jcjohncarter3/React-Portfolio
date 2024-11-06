4aa8d3092d3d3e0be77ce624c9143c47e7f7a624
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-05 21:54:49 -0600

    added text and error messages

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,22 +1,88 @@
-// This is a static page mocking an "About Us" section for our fake user data
+import { useState, useEffect } from "react";
 export default function ContactPage() {
+    const [showError, setShowError] = useState(false);
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {
+      if (!fullName && !email && !message) {
+        setShowError(true)
+        setErrorMessage("Fill out the whole form!")
+      }
+      else if (!fullName && email && message) {
+        setShowError(true)
+        setErrorMessage("Please enter your name!")
+      } else if (!email && fullName && message) {
+        setShowError(true)
+        setErrorMessage("Please enter your email!")
+      } else if (!message && fullName && email) {
+        setShowError(true)
+        setErrorMessage("Please enter a message!")
+      }
+      else {
+        if (email && email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+          setShowError(false)
+        } else {
+          setShowError(true)
+          setErrorMessage("Please enter your email!")
+        }
+      }
+      
+    }, [fullName, email, message])
+
+    function messageField(event) {
+       setMessage(event.target.value)
+    }
+
+    function fullNameChange(event) {
+      setFullName(event.target.value)
+    }
+
+    function emailChange(event) {
+      setEmail(event.target.value)
+    }
+    
     return (
       <div className="page">
         <h2>Contact</h2>
         <form>
           <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Full Name</label>
-            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+            <label htmlFor="fullName" className="form-label">Full Name</label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="fullName" 
+              value={fullName}
+              onChange={event => fullNameChange(event)}
+            />
           </div>
           <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Email</label>
-            <input type="email" className="form-control" id="exampleInputPassword1"/>
+            <label htmlFor="email" className="form-label">Email</label>
+            <input 
+              type="email" 
+              className="form-control" 
+              id="email"
+              value={email}
+              onChange={event => emailChange(event)}
+            />
           </div>
           <div className="mb-3 ">
-          <label htmlFor="messageField" className="form-label">Message</label>
-          <textarea className="form-control" placeholder="Leave a comment here" id="messageField"></textarea>
+          <label htmlFor="message" className="form-label">Message</label>
+              <textarea 
+                className="form-control" 
+                placeholder="Leave a comment here" 
+                id="message"
+                value={message}
+                onChange={event => messageField(event)}>
+              </textarea>
           </div>
+          {showError && <div className="form-group pb-2">
+            <small className="form-text form-error-message">
+              {errorMessage}
+            </small>
+          </div>}
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
